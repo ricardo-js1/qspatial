3200f1ef25933a5dba318d332251704672cd5369
qspatial <pacoterespacial@gmail.com>
2019-02-19 13:32:48 -0300

    plotgg function

--- R/plotgg.R
+++ R/plotgg.R
@@ -4,12 +4,12 @@ plotgg = function(shapefile = "shapefile", variable = "adata", maptitle = NULL,
   if(continuous == TRUE){
 
     cat("Use this to plot the map with ggplot2:
-      ggplot(",shapefile,")+
-      geom_polygon(aes(x = long, y = lat, group = group, fill ='",variable,"'), col =","black) +
-      xlab(","Longitude) +
-      ylab(Latitude) +
-      scale_fill_viridis_c() +
-      ggtitle('",maptitle,"')")
+         ggplot(",shapefile,")+
+         geom_polygon(aes(x = long, y = lat, group = group, fill ='",variable,"'), col =","black) +
+         xlab(","Longitude) +
+         ylab(Latitude) +
+         scale_fill_viridis_c() +
+         ggtitle('",maptitle,"')")
 
   } else {
 
