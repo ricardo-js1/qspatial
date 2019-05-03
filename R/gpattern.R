5991cfd499ff9b5919fdb4a81ced7ea821d7855c
qspatial <pacoterespacial@gmail.com>
2019-05-03 18:46:45 -0300

    Tweaks

--- R/gpattern.R
+++ R/gpattern.R
@@ -1,14 +1,22 @@
 
 gpattern = function(shapefile, longitude = longitude, latitude = latitude, nsim = 99){
+
+  # Creating the ppp object
   pattern = spatstat::ppp(longitude, latitude, window = spatstat::as.owin(shapefile))
+  pattern = spatstat::as.ppp(pattern) # removing the events outside the specified window
+  pattern = spatstat::unique.ppp(pattern) # removing duplicates
+
+  # CSR envelope for the G function
+
   genv = spatstat::envelope(pattern, Gest, nsim = nsim)
   ggplot2::ggplot(data.frame(genv), aes(x = r)) +
     ggplot2::ggtitle("G function") +
     ggplot2::geom_line(aes(y = lo), col='grey') +
     ggplot2::geom_line(aes(y = hi), col='grey') +
-    ggplot2::geom_ribbon(aes(ymin = lo, ymax = hi), alpha = 0.5, col='grey') +
-    ggplot2::geom_line(aes(y = obs)) +
+    ggplot2::geom_ribbon(aes(ymin = lo - 0.01, ymax = hi + 0.01), alpha = 0.75, fill = 'grey') +
     ggplot2::geom_line(aes(y = theo), col = 'red') +
+    ggplot2::geom_line(aes(y = obs)) +
     ggplot2::ylab("G(r)") + ggplot2::xlab("r") +
     theme_qspatial()
 }
+
--- R/jpattern.R
+++ R/jpattern.R
@@ -3,17 +3,24 @@
 #' Plots the J-Function envelope for point pattern data.
 
 jpattern = function(shapefile, longitude = longitude, latitude = latitude, nsim = 99){
+
+  # Creating the ppp object
   pattern = spatstat::ppp(longitude, latitude, window = spatstat::as.owin(shapefile))
+  pattern = spatstat::as.ppp(pattern) # removing the events outside the specified window
+  pattern = spatstat::unique.ppp(pattern) # removing duplicates
+
+  # CSR envelope for the J function
+
   jenv = spatstat::envelope(pattern, Jest, nsim = nsim)
-  ggplot2::ggplot(data.frame(jenv), aes(x = r))+
-    ggplot2::geom_line(aes(y = lo), col='grey') +
-    ggplot2::geom_line(aes(y = hi), col='grey') +
-    ggplot2::geom_ribbon(aes(ymin = lo, ymax = hi), alpha = 0.5, col='grey') +
-    ggplot2::geom_line(aes(y = obs)) +
-    ggplot2::geom_line(aes(y = theo), col = 'red') +
-    ggplot2::ggtitle("J function") +
-    ggplot2::ylab("J(r)") + ggplot2::xlab("r") +
-    theme_qspatial()
+  ggplot2::ggplot(data.frame(jenv), aes(x = r)) +
+  ggplot2::geom_line(aes(y = lo), col='grey') +
+  ggplot2::geom_line(aes(y = hi), col='grey') +
+  ggplot2::geom_ribbon(aes(ymin = lo - 0.01, ymax = hi + 0.01), alpha = 0.75, fill = 'grey') +
+  ggplot2::geom_line(aes(y = theo), col = 'red') +
+  ggplot2::geom_line(aes(y = obs)) +
+  ggplot2::ggtitle("J function") +
+  ggplot2::ylab("J(r)") + ggplot2::xlab("r") +
+  theme_qspatial()
 
 }
 
