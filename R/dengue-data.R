843f797f86d881051d9b3440a8efc5b7c8a22a0b
qspatial <pacoterespacial@gmail.com>
2019-05-03 21:29:26 -0300

    Documentation for the dengue dataset

---
+++ R/dengue-data.R
@@ -0,0 +1,18 @@
+#' Dengue cases counts on the state of Rio de Janeiro from 2009 to 2013
+#'
+#' The data consists of the number of notifications of Dengue fever on the
+#' cities of Rio de Janeiro state in Brazil. The data is divided by
+#' year from 2009 to 2013.
+#'
+#' @docType data
+#'
+#' @usage data(dengue)
+#'
+#' @format An object of class data.frame.
+#'
+#' @keywords datasets
+#'
+#' @source \href{http://tabnet.datasus.gov.br/cgi/tabcgi.exe?sinannet/cnv/denguebr.def}{Datasus}
+#'
+#' @examples
+#' data(dengue)
