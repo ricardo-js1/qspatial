af8f701a2c4317f5a0dd95d4ed934ffb02fe0d22
qspatial <pacoterespacial@gmail.com>
2019-05-05 16:38:43 -0300

    Rioshapefile documentation

---
+++ R/rioshapefile-data.R
@@ -0,0 +1,16 @@
+#' A Rio de Janeiro state shapefile divided by cities
+#'
+#' This is a shapefile of Rio de Janeiro State in Brazil. It's divided by
+#' cities and contains all of it's 92 cities as of May 2019.
+#'
+#'
+#' @docType data
+#'
+#' @usage data(rioshapefile)
+#'
+#' @format An object of class SpatialPolygonsDataFrame
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(rioshapefile)
