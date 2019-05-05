39df5704a15bbc9eb098629438d36b6e88794eb4
qspatial <pacoterespacial@gmail.com>
2019-05-05 19:09:19 -0300

    Example data for point pattern functions and documentation

---
+++ R/accidents-data.R
@@ -0,0 +1,18 @@
+#' Transit accident data of Recife in 2016
+#'
+#' The data contains the coordinates of transit accidents with that victims
+#' happened in Recife during 2016. The coordinates used here are latitude
+#' and longitude.
+#'
+#' @docType data
+#'
+#' @usage data(accidents)
+#'
+#' @format An object of class data.frame.
+#'
+#' @keywords datasets
+#'
+#' @source \href{http://dados.recife.pe.gov.br/en/dataset/acidentes-de-transito-c-vitimas-2016}{Data Recife}
+#'
+#' @examples
+#' data(accidents)
---
+++ R/recifeshapefile-data.R
@@ -0,0 +1,15 @@
+#' A Recife shapefile divided by neighborhoods
+#'
+#' This is a shapefile of Recife city in Brazil.
+#'
+#'
+#' @docType data
+#'
+#' @usage data(recifeshapefile)
+#'
+#' @format An object of class SpatialPolygonsDataFrame
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(recifeshapefile)
