d3c0a20185d6a494ab93b9083e7fad1383a07374
qspatial <pacoterespacial@gmail.com>
2019-08-17 01:12:35 -0300

    bugfixes

--- R/areamap.R
+++ R/areamap.R
@@ -54,13 +54,15 @@ areamap = function(shapefile = shapefile, adata = data, maptitle = "Map Title", 
   if(length(shapefile) != length(adata)){stop("The length of the data vector must be the same as the number of polygons in the shapefile.")}
 
   # Joining the data and converting the spatial object to a dataframe to be compatible with ggplot2
-  warning.status <- getOption("warn")
-  options(warn = -1)
-  shapefile$adata = adata
-  shapefile.df = broom::tidy(shapefile, regions = "id")
-  shapefile$id = rownames(shapefile@data)
-  shapefile.df = dplyr::left_join(shapefile.df, shapefile@data, by = "id")
-  options(warn = warning.status)
+  #warning.status <- getOption("warn")
+  #options(warn = -1)
+  #shapefile$adata = adata
+  #shapefile.df = broom::tidy(shapefile, regions = "id")
+  #shapefile$id = rownames(shapefile@data)
+  #shapefile.df = dplyr::left_join(shapefile.df, shapefile@data, by = "id")
+  #options(warn = warning.status)
+
+  shapefile.sf = as(shapefile, "sf")
 
   # Plotting the map
 
@@ -70,22 +72,20 @@ areamap = function(shapefile = shapefile, adata = data, maptitle = "Map Title", 
     if(is.null(lower)){lower = min(adata)}
     if(is.null(upper)){upper = max(adata)}
 
-    ggplot2::ggplot(shapefile.df) +
-      ggplot2::geom_polygon(ggplot2::aes(x = long, y = lat, group = group, fill = adata), col = "black") +
+    ggplot2::ggplot() +
+      ggplot2::geom_sf(data = shapefile.sf, aes(fill = adata), col = "black") +
       ggplot2::xlab("Longitude") + ggplot2::ylab("Latitude") +
       ggplot2::scale_fill_viridis_c(name = guidetitle, limits = c(lower, upper))+
       ggplot2::ggtitle(maptitle) +
-      ggplot2::coord_map() +
       theme_qspatial()
 
   } else {
 
-    ggplot2::ggplot(shapefile.df) +
-      ggplot2::geom_polygon(ggplot2::aes(x = long, y = lat, group = group, fill = adata), col = "black") +
+    ggplot2::ggplot() +
+      ggplot2::geom_sf(data = shapefile.sf, aes(fill = adata), col = "black") +
       ggplot2::xlab("Longitude") + ggplot2::ylab("Latitude") +
       ggplot2::scale_fill_viridis_d(name = guidetitle, drop = FALSE)+
       ggplot2::ggtitle(maptitle) +
-      ggplot2::coord_map() +
       theme_qspatial()
 
   }
